d0094afd76b6a6c7eee2dd9a0beec39d8a578a93
bappi <bappionfire261@gmail.com>
2026-03-01 11:18:40 +0600

    set the donation.js file done

---
+++ js/donation.js
@@ -0,0 +1,27 @@
+
+document.getElementById('donate-btn').addEventListener('click', function (event) {
+   event.preventDefault();
+
+    const inputDonation = getInputFieldValueById ('input-donation');
+     const donationMoney = getTextValueById('donation-money');
+   const priceMoney = getTextValueById('danation-price');
+    console.log(priceMoney)
+
+
+    if(isNaN(inputDonation) ){
+        alert('Please enter a valid inputDonation');
+        return;
+    }
+    if(inputDonation > donationMoney){
+        alert('Not enough balance!')
+        return;
+    };
+     
+    const newBalance = donationMoney - inputDonation;
+    document.getElementById('donation-money').innerText = newBalance;
+
+    // set the donation price
+    const totalPrice = inputDonation + priceMoney;
+    document.getElementById('danation-price').innerText = totalPrice;
+    
+})
